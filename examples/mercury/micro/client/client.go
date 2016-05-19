19266c9ebf3c11984008a02160f3f4c4dc7e76f9
Asim <asim@chuhnk.me>
2016-05-19 16:57:04 +0100

    Move mercury to named selector which is more useful

--- examples/mercury/micro/client/client.go
+++ examples/mercury/micro/client/client.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/micro/go-micro/client"
 	mcodec "github.com/micro/go-plugins/codec/mercury"
-	"github.com/micro/go-plugins/selector/mercury"
+	"github.com/micro/go-plugins/selector/named"
 	"github.com/micro/go-plugins/transport/rabbitmq"
 	hello "github.com/micro/micro/examples/greeter/server/proto/hello"
 
@@ -17,7 +17,7 @@ func main() {
 	rabbitmq.DefaultRabbitURL = "amqp://localhost:5672"
 
 	c := client.NewClient(
-		client.Selector(mercury.NewSelector()),
+		client.Selector(named.NewSelector()),
 		client.Transport(rabbitmq.NewTransport()),
 		client.Codec("application/x-protobuf", mcodec.NewCodec),
 		client.ContentType("application/x-protobuf"),
---
+++ selector/named/doc.go
@@ -0,0 +1,7 @@
+package named
+
+/*
+	Named selector returns the named service as a node for every request.
+	This is useful where you want to offload discovery and balancing too
+	a message bus.
+*/
