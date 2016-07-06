9481c53312f0093bf12c89b3e13109436291ac13
Philipp Brüll <bruell@simiatech.com>
2016-07-06 16:52:37 +0200

    removed test dependency

--- registry/nats/nats_test.go
+++ registry/nats/nats_test.go
@@ -3,23 +3,21 @@ package nats_test
 import (
 	"testing"
 
-	"code.posteo.de/go-common/test/assert"
-
 	"github.com/micro/go-micro/registry"
 )
 
 func TestRegister(t *testing.T) {
 	service := registry.Service{Name: "test"}
-	assert.NoError(t, e.registryOne.Register(&service))
+	assertNoError(t, e.registryOne.Register(&service))
 	defer e.registryOne.Deregister(&service)
 
 	services, err := e.registryOne.ListServices()
-	assert.NoError(t, err)
-	assert.Equal(t, 3, len(services))
+	assertNoError(t, err)
+	assertEqual(t, 3, len(services))
 
 	services, err = e.registryTwo.ListServices()
-	assert.NoError(t, err)
-	assert.Equal(t, 3, len(services))
+	assertNoError(t, err)
+	assertEqual(t, 3, len(services))
 }
 
 func TestDeregister(t *testing.T) {
@@ -27,63 +25,63 @@ func TestDeregister(t *testing.T) {
 
 	service := registry.Service{Name: "test"}
 
-	assert.NoError(t, e.registryOne.Register(&service))
-	assert.NoError(t, e.registryOne.Deregister(&service))
+	assertNoError(t, e.registryOne.Register(&service))
+	assertNoError(t, e.registryOne.Deregister(&service))
 
 	services, err := e.registryOne.ListServices()
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(services))
+	assertNoError(t, err)
+	assertEqual(t, 0, len(services))
 
 	services, err = e.registryTwo.ListServices()
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(services))
+	assertNoError(t, err)
+	assertEqual(t, 0, len(services))
 }
 
 func TestGetService(t *testing.T) {
 	services, err := e.registryTwo.GetService("one")
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(services))
-	assert.Equal(t, "one", services[0].Name)
-	assert.Equal(t, 1, len(services[0].Nodes))
+	assertNoError(t, err)
+	assertEqual(t, 1, len(services))
+	assertEqual(t, "one", services[0].Name)
+	assertEqual(t, 1, len(services[0].Nodes))
 }
 
 func TestGetServiceWithNoNodes(t *testing.T) {
 	services, err := e.registryOne.GetService("missing")
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(services))
+	assertNoError(t, err)
+	assertEqual(t, 0, len(services))
 }
 
 func TestGetServiceFromMultipleNodes(t *testing.T) {
 	services, err := e.registryOne.GetService("two")
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(services))
-	assert.Equal(t, "two", services[0].Name)
-	assert.Equal(t, 2, len(services[0].Nodes))
+	assertNoError(t, err)
+	assertEqual(t, 1, len(services))
+	assertEqual(t, "two", services[0].Name)
+	assertEqual(t, 2, len(services[0].Nodes))
 }
 
 func BenchmarkGetService(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		services, err := e.registryTwo.GetService("one")
-		assert.NoError(b, err)
-		assert.Equal(b, 1, len(services))
-		assert.Equal(b, "one", services[0].Name)
+		assertNoError(b, err)
+		assertEqual(b, 1, len(services))
+		assertEqual(b, "one", services[0].Name)
 	}
 }
 
 func BenchmarkGetServiceWithNoNodes(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		services, err := e.registryOne.GetService("missing")
-		assert.NoError(b, err)
-		assert.Equal(b, 0, len(services))
+		assertNoError(b, err)
+		assertEqual(b, 0, len(services))
 	}
 }
 
 func BenchmarkGetServiceFromMultipleNodes(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		services, err := e.registryTwo.GetService("two")
-		assert.NoError(b, err)
-		assert.Equal(b, 1, len(services))
-		assert.Equal(b, "two", services[0].Name)
-		assert.Equal(b, 2, len(services[0].Nodes))
+		assertNoError(b, err)
+		assertEqual(b, 1, len(services))
+		assertEqual(b, "two", services[0].Name)
+		assertEqual(b, 2, len(services[0].Nodes))
 	}
 }
