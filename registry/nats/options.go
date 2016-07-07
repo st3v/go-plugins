cd9e31eb6716783f6552630525363652cfc5108f
Philipp Brüll <bruell@simiatech.com>
2016-07-07 14:29:15 +0200

    small change in registry nats options

--- registry/nats/options.go
+++ registry/nats/options.go
@@ -5,13 +5,13 @@ import (
 	"golang.org/x/net/context"
 )
 
-const quorumKey = "quorum"
+type contextQuorumKey struct{}
 
 var DefaultQuorum = 0
 
 func Quorum(n int) registry.Option {
 	return func(o *registry.Options) {
-		o.Context = context.WithValue(o.Context, quorumKey, n)
+		o.Context = context.WithValue(o.Context, contextQuorumKey{}, n)
 	}
 }
 
@@ -20,7 +20,7 @@ func getQuorum(o registry.Options) int {
 		return DefaultQuorum
 	}
 
-	value := o.Context.Value(quorumKey)
+	value := o.Context.Value(contextQuorumKey{})
 	if v, ok := value.(int); ok {
 		return v
 	} else {
