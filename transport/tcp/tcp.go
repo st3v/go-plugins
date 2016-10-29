5f2cfe7449875ae2027afd9a3275246627066db6
Asim <asim@chuhnk.me>
2016-10-29 18:15:40 +0100

    TCP transport using gob encoder

---
+++ transport/tcp/tcp.go
@@ -0,0 +1,350 @@
+package transport
+
+import (
+	"bufio"
+	"crypto/tls"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/micro/go-micro/cmd"
+	"github.com/micro/go-micro/transport"
+	mls "github.com/micro/misc/lib/tls"
+)
+
+type tcpTransport struct {
+	opts transport.Options
+}
+
+type tcpTransportClient struct {
+	dialOpts transport.DialOptions
+	conn     net.Conn
+	enc      *gob.Encoder
+	dec      *gob.Decoder
+	encBuf   *bufio.Writer
+	timeout  time.Duration
+}
+
+type tcpTransportSocket struct {
+	conn    net.Conn
+	enc     *gob.Encoder
+	dec     *gob.Decoder
+	encBuf  *bufio.Writer
+	timeout time.Duration
+}
+
+type tcpTransportListener struct {
+	listener net.Listener
+	timeout  time.Duration
+}
+
+func init() {
+	cmd.DefaultTransports["tcp"] = NewTransport
+}
+
+func getIPAddrs() []string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil
+	}
+
+	var ipAddrs []string
+
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			continue
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			if ip == nil {
+				continue
+			}
+
+			ip = ip.To4()
+			if ip == nil {
+				continue
+			}
+
+			ipAddrs = append(ipAddrs, ip.String())
+		}
+	}
+
+	return ipAddrs
+}
+
+func listen(addr string, fn func(string) (net.Listener, error)) (net.Listener, error) {
+	// host:port || host:min-max
+	parts := strings.Split(addr, ":")
+
+	//
+	if len(parts) < 2 {
+		return fn(addr)
+	}
+
+	// try to extract port range
+	ports := strings.Split(parts[len(parts)-1], "-")
+
+	// single port
+	if len(ports) < 2 {
+		return fn(addr)
+	}
+
+	// we have a port range
+
+	// extract min port
+	min, err := strconv.Atoi(ports[0])
+	if err != nil {
+		return nil, errors.New("unable to extract port range")
+	}
+
+	// extract max port
+	max, err := strconv.Atoi(ports[1])
+	if err != nil {
+		return nil, errors.New("unable to extract port range")
+	}
+
+	// set host
+	host := parts[:len(parts)-1]
+
+	// range the ports
+	for port := min; port <= max; port++ {
+		// try bind to host:port
+		ln, err := fn(fmt.Sprintf("%s:%d", host, port))
+		if err == nil {
+			return ln, nil
+		}
+
+		// hit max port
+		if port == max {
+			return nil, err
+		}
+	}
+
+	// why are we here?
+	return nil, fmt.Errorf("unable to bind to %s", addr)
+}
+
+func (t *tcpTransportClient) Send(m *transport.Message) error {
+	// set timeout if its greater than 0
+	if t.timeout > time.Duration(0) {
+		t.conn.SetDeadline(time.Now().Add(t.timeout))
+	}
+	if err := t.enc.Encode(m); err != nil {
+		return err
+	}
+	return t.encBuf.Flush()
+}
+
+func (t *tcpTransportClient) Recv(m *transport.Message) error {
+	// set timeout if its greater than 0
+	if t.timeout > time.Duration(0) {
+		t.conn.SetDeadline(time.Now().Add(t.timeout))
+	}
+	return t.dec.Decode(&m)
+}
+
+func (t *tcpTransportClient) Close() error {
+	return t.conn.Close()
+}
+
+func (t *tcpTransportSocket) Recv(m *transport.Message) error {
+	if m == nil {
+		return errors.New("message passed in is nil")
+	}
+
+	// set timeout if its greater than 0
+	if t.timeout > time.Duration(0) {
+		t.conn.SetDeadline(time.Now().Add(t.timeout))
+	}
+
+	return t.dec.Decode(&m)
+}
+
+func (t *tcpTransportSocket) Send(m *transport.Message) error {
+	// set timeout if its greater than 0
+	if t.timeout > time.Duration(0) {
+		t.conn.SetDeadline(time.Now().Add(t.timeout))
+	}
+	if err := t.enc.Encode(m); err != nil {
+		return err
+	}
+	return t.encBuf.Flush()
+}
+
+func (t *tcpTransportSocket) Close() error {
+	return t.conn.Close()
+}
+
+func (t *tcpTransportListener) Addr() string {
+	return t.listener.Addr().String()
+}
+
+func (t *tcpTransportListener) Close() error {
+	return t.listener.Close()
+}
+
+func (t *tcpTransportListener) Accept(fn func(transport.Socket)) error {
+	var tempDelay time.Duration
+
+	for {
+		c, err := t.listener.Accept()
+		if err != nil {
+			if ne, ok := err.(net.Error); ok && ne.Temporary() {
+				if tempDelay == 0 {
+					tempDelay = 5 * time.Millisecond
+				} else {
+					tempDelay *= 2
+				}
+				if max := 1 * time.Second; tempDelay > max {
+					tempDelay = max
+				}
+				log.Printf("http: Accept error: %v; retrying in %v\n", err, tempDelay)
+				time.Sleep(tempDelay)
+				continue
+			}
+			return err
+		}
+
+		encBuf := bufio.NewWriter(c)
+		sock := &tcpTransportSocket{
+			timeout: t.timeout,
+			conn:    c,
+			encBuf:  encBuf,
+			enc:     gob.NewEncoder(encBuf),
+			dec:     gob.NewDecoder(c),
+		}
+
+		go func() {
+			// TODO: think of a better error response strategy
+			defer func() {
+				if r := recover(); r != nil {
+					sock.Close()
+				}
+			}()
+
+			fn(sock)
+		}()
+	}
+}
+
+func (t *tcpTransport) Dial(addr string, opts ...transport.DialOption) (transport.Client, error) {
+	dopts := transport.DialOptions{
+		Timeout: transport.DefaultDialTimeout,
+	}
+
+	for _, opt := range opts {
+		opt(&dopts)
+	}
+
+	var conn net.Conn
+	var err error
+
+	// TODO: support dial option here rather than using internal config
+	if t.opts.Secure || t.opts.TLSConfig != nil {
+		config := t.opts.TLSConfig
+		if config == nil {
+			config = &tls.Config{
+				InsecureSkipVerify: true,
+			}
+		}
+		conn, err = tls.DialWithDialer(&net.Dialer{Timeout: dopts.Timeout}, "tcp", addr, config)
+	} else {
+		conn, err = net.DialTimeout("tcp", addr, dopts.Timeout)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	encBuf := bufio.NewWriter(conn)
+
+	return &tcpTransportClient{
+		dialOpts: dopts,
+		conn:     conn,
+		encBuf:   encBuf,
+		enc:      gob.NewEncoder(encBuf),
+		dec:      gob.NewDecoder(conn),
+		timeout:  t.opts.Timeout,
+	}, nil
+}
+
+func (t *tcpTransport) Listen(addr string, opts ...transport.ListenOption) (transport.Listener, error) {
+	var options transport.ListenOptions
+	for _, o := range opts {
+		o(&options)
+	}
+
+	var l net.Listener
+	var err error
+
+	// TODO: support use of listen options
+	if t.opts.Secure || t.opts.TLSConfig != nil {
+		config := t.opts.TLSConfig
+
+		fn := func(addr string) (net.Listener, error) {
+			if config == nil {
+				hosts := []string{addr}
+
+				// check if its a valid host:port
+				if host, _, err := net.SplitHostPort(addr); err == nil {
+					if len(host) == 0 {
+						hosts = getIPAddrs()
+					} else {
+						hosts = []string{host}
+					}
+				}
+
+				// generate a certificate
+				cert, err := mls.Certificate(hosts...)
+				if err != nil {
+					return nil, err
+				}
+				config = &tls.Config{Certificates: []tls.Certificate{cert}}
+			}
+			return tls.Listen("tcp", addr, config)
+		}
+
+		l, err = listen(addr, fn)
+	} else {
+		fn := func(addr string) (net.Listener, error) {
+			return net.Listen("tcp", addr)
+		}
+
+		l, err = listen(addr, fn)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &tcpTransportListener{
+		timeout:  t.opts.Timeout,
+		listener: l,
+	}, nil
+}
+
+func (t *tcpTransport) String() string {
+	return "tcp"
+}
+
+func NewTransport(opts ...transport.Option) transport.Transport {
+	var options transport.Options
+	for _, o := range opts {
+		o(&options)
+	}
+	return &tcpTransport{opts: options}
+}
