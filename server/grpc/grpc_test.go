c9522d0d3ef5635227490068dd7e8d9136e22476
Asim Aslam <asim@chuhnk.me>
2017-01-05 14:25:02 +0000

    update examples link

--- server/grpc/grpc_test.go
+++ server/grpc/grpc_test.go
@@ -8,7 +8,7 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
-	pb "github.com/micro/micro/examples/greeter/server/proto/hello"
+	pb "github.com/micro/examples/greeter/server/proto/hello"
 )
 
 // server is used to implement helloworld.GreeterServer.
