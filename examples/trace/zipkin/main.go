b9a897f477f11b53d96422f5476ab281460a6101
Asim Aslam <asim@chuhnk.me>
2017-01-12 22:18:39 +0000

    go fmt

--- examples/trace/zipkin/main.go
+++ examples/trace/zipkin/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	example "github.com/micro/examples/server/proto/example"
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
-	example "github.com/micro/examples/server/proto/example"
 	"github.com/micro/go-micro/metadata"
 	"github.com/micro/go-os/trace"
 	"github.com/micro/go-plugins/trace/zipkin"
